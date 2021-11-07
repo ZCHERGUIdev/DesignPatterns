fd7671cdaeb5750bdb44955d07e8fac3e5c1d730
zakaria chergui <z.chergui@esi-sba.dz>
2021-11-07 18:50:17 +0100

    firstcommint

---
+++ app/src/main/java/com/zcdev/designpatternsandroiddev/factory/DialogFactory.kt
@@ -0,0 +1,4 @@
+package com.zcdev.designpatternsandroiddev.factory
+
+class DialogFactory {
+}
---
+++ app/src/main/java/com/zcdev/designpatternsandroiddev/singlton/Singlton.kt
@@ -0,0 +1,16 @@
+package com.zcdev.designpatternsandroiddev.singlton
+
+object Singlton {
+
+    // one object of class == 1 instance i the app
+    /*
+    if you want know how to create singlton using java
+    go to tools -> kotlin -> show kotlin byte code -> decompile
+     */
+
+    fun doSomeThing(){
+        // some code
+    }
+
+
+}
